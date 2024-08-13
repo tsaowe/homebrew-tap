0ac5da26c475a000dcc012539ce36fcef8894628
caowei <tsaowe@gmail.com>
2024-08-13 11:54:35 +0800

    update node

--- Formula/nice-import.rb
+++ Formula/nice-import.rb
@@ -8,7 +8,7 @@ class NiceImport < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
